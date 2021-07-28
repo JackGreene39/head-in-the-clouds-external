582455b254e0337ac902677f5919a2e7faadf0c2
naveenachilukuri <naveenachilukuri@yahoo.com>
2021-07-28 16:33:10 +0000

    updated test

--- test/test-server.js
+++ test/test-server.js
@@ -27,7 +27,7 @@ describe('GET /', function () {
         if (err) {
           return done(err);
         }
-        chai.assert.isTrue(res.text.includes("<h1Welcomeeee to Head in the Clouds application</h1>"));
+        chai.assert.isTrue(res.text.includes("<h1>Welcomeeee to Head in the Clouds application</h1>"));
         return done();
       });
 
